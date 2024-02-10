ae203ea6739a914a7cd2c01a7ba162510e574d75
Anuradha <anuradhaah667@gmail.com>
2024-02-10 10:01:09 +0530

    added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+        RWOnly r = new RWOnly();
+//        r.name = "Anuradha";
+//        System.out.println(r.name);
+        r.setName("Anuradha");
+        System.out.println(r.getName());
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    public void setName(String s){
+        this.name = s;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
